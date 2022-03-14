16765a804c715ab83a94478fb170befc271b57c8
PrajwalCH <prajjwal2058@gmail.com>
2022-03-14 08:31:08 +0545

    feat: implementation of MatchedFlag.zig

---
+++ src/MatchedFlag.zig
@@ -0,0 +1,28 @@
+const MatchedFlag = @This();
+
+pub const Arg = union(enum) {
+    none: bool,
+    single: []const u8,
+};
+
+name: []const u8,
+arg: Arg,
+
+fn init(name: []const u8) MatchedFlag {
+    return MatchedFlag{
+        .name = name,
+        .arg = undefined,
+    };
+}
+
+pub fn initWithoutArg(name: []const u8) MatchedFlag {
+    var self = init(name);
+    self.arg = .{ .none = true };
+    return self;
+}
+
+pub fn initWithArg(name: []const u8, arg: []const u8) MatchedFlag {
+    var self = init(name);
+    self.arg = .{ .single = arg };
+    return self;
+}
