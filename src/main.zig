3fe942d747594dd7d66fa9bbc800b10eb6e87762
PrajwalCH <prajjwal2058@gmail.com>
2022-03-19 17:06:35 +0545

    feat: add complete (kind of) test

--- src/main.zig
+++ src/main.zig
@@ -3,32 +3,65 @@ const Command = @import("Command.zig");
 const Flag = @import("Flag.zig");
 const testing = std.testing;
 
-test "command parser" {
-    const allocator = std.heap.page_allocator;
+const allocator = std.heap.page_allocator;
 
-    var cmd = Command.new(allocator, "test");
-    try cmd.flag(Flag.ArgOne("--gen-makefile"));
-    try cmd.subCommand(Command.new(allocator, "help"));
-    try cmd.subCommand(Command.new(allocator, "clean"));
+fn initFakeCliArgs(alloc: std.mem.Allocator) !Command {
+    var root_cmd = Command.new(alloc, "zig-arg");
 
-    var compile_cmd = Command.new(allocator, "compile");
+    try root_cmd.flag(Flag.Bool("--version"));
+    try root_cmd.flag(Flag.ArgOne("--compile-only"));
+    try root_cmd.subCommand(Command.new(alloc, "help"));
+
+    var compile_cmd = Command.new(alloc, "compile");
     try compile_cmd.flag(Flag.Option("--mode", &[_][]const u8{
-        "debug",
         "release",
+        "debug",
     }));
 
-    compile_cmd.flagRequired(true);
+    try root_cmd.subCommand(compile_cmd);
+    return root_cmd;
+}
 
-    try cmd.subCommand(compile_cmd);
-
+test "flag required error" {
     const argv: []const [:0]const u8 = &.{
         "compile",
         "--mode",
+        "debug",
     };
 
-    var matches = try cmd.parse(argv);
+    var root_cmd = try initFakeCliArgs(allocator);
+    root_cmd.flagRequired(true);
 
-    if (matches.subcommandMatches("build")) |build_cmd_matches| {
-        try testing.expectEqualSlices(u8, "debug", build_cmd_matches.valueOf("--mode").?);
+    try testing.expectError(error.MissingCommandFlags, root_cmd.parse(argv));
+}
+
+test "subcommand required error" {
+    const argv: []const [:0]const u8 = &.{
+        "--version",
+    };
+
+    var root_cmd = try initFakeCliArgs(allocator);
+    root_cmd.subcommandRequired(true);
+
+    try testing.expectError(error.MissingCommandArgument, root_cmd.parse(argv));
+}
+
+test "full parsing" {
+    const argv: []const [:0]const u8 = &.{
+        "compile",
+        "--mode",
+        "debug",
+    };
+
+    var root_cmd = try initFakeCliArgs(allocator);
+    root_cmd.subcommandRequired(true);
+
+    var matches = try root_cmd.parse(argv);
+
+    try testing.expectEqual(false, matches.isPresent("--version"));
+
+    if (matches.subcommandMatches("compile")) |compile_cmd_matches| {
+        try testing.expectEqual(true, compile_cmd_matches.isPresent("--mode"));
+        try testing.expectEqualStrings("debug", compile_cmd_matches.valueOf("--mode").?);
     }
 }
