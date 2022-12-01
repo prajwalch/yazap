2224120006e810c892968cf9fee5b39ddf2294db
PrajwalCH <prajjwal2058@gmail.com>
2022-12-02 00:22:42 +0545

    fix(lib): Update args_context import statement

--- src/lib.zig
+++ src/lib.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 pub const flag = @import("flag.zig");
+pub const args_context = @import("args_context.zig");
 pub const Arg = @import("Arg.zig");
 pub const Command = @import("Command.zig");
-pub const ArgsContext = @import("parser/ArgsContext.zig");
 pub const Yazap = @import("Yazap.zig");
 
 test "emit docs" {
