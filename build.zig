cac540d18f7addd3d35f8d294e1506ebc71be667
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-01-14 10:40:48 +0545

    feat(build): Add `examples` step to build all the examples

--- build.zig
+++ build.zig
@@ -1,11 +1,12 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("arg", "src/main.zig");
+    const lib = b.addStaticLibrary("yazap", "src/main.zig");
     lib.setBuildMode(mode);
     lib.install();
 
@@ -20,4 +21,19 @@ pub fn build(b: *std.build.Builder) void {
 
     const docs_step = b.step("docs", "Generate docs");
     docs_step.dependOn(&docs.step);
+
+    const examples_step = b.step("examples", "Build all the example");
+
+    inline for (.{
+        "git",
+        "touch",
+    }) |example_name| {
+        const example_exe = b.addExecutable(example_name, "examples/" ++ example_name ++ ".zig");
+        example_exe.setTarget(target);
+        example_exe.setBuildMode(mode);
+        example_exe.addPackagePath("yazap", "src/lib.zig");
+        example_exe.install();
+
+        examples_step.dependOn(b.getInstallStep());
+    }
 }
