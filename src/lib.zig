c3103b239e65bdca22a69787f48e5d5063b29c8a
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-01-28 10:51:32 +0545

    feat(lib): Expose `error.zig`

--- src/lib.zig
+++ src/lib.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+pub const err = @import("error.zig");
 pub const flag = @import("flag.zig");
 pub const args_context = @import("args_context.zig");
 pub const App = @import("App.zig");
