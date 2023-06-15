b5974d833fde62b3945d275e1b884d678cbade57
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-06-15 23:03:57 +0545

    feat(Arg): Improve example in doc comment of `positional`
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/Arg.zig
+++ src/Arg.zig
@@ -147,15 +147,20 @@ pub fn multiArgumentsOptionWithValidValues(
 /// NOTE: Index is optional so by default it will be assigned in order of evalution.
 ///
 /// ```zig
-/// Order dependent
-/// try root.addArg(Arg.positional("ONE", null, null));
-/// try root.addArg(Arg.positional("TWO", null, null));
-/// try root.addArg(Arg.positional("THREE", null, null));
+/// var app = App.init(allocator, "myapp", "My app description");
+/// defer app.deinit();
+///
+/// var root = app.rootCommand();
+///
+/// // Order dependent
+/// try root.addArg(Arg.positional("FIRST", null, null));
+/// try root.addArg(Arg.positional("SECOND", null, null));
+/// try root.addArg(Arg.positional("THIRD", null, null));
 ///
 /// // Equivalent but order independent
-/// try root.addArg(Arg.positional("THREE", null, 3));
-/// try root.addArg(Arg.positional("TWO", null, 2));
-/// try root.addArg(Arg.positional("ONE", null, 1));
+/// try root.addArg(Arg.positional("THIRD", null, 3));
+/// try root.addArg(Arg.positional("SECOND", null, 2));
+/// try root.addArg(Arg.positional("FIRST", null, 1));
 /// ```
 pub fn positional(name: []const u8, description: ?[]const u8, index: ?usize) Arg {
     var arg = Arg.init(name, description);
