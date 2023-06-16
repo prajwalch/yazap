be575a654f46ff96844348f8ee37c1bc9bd86cd1
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-06-16 16:16:46 +0545

    refactor: Only expose `YazapError` from `error.zig`
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/lib.zig
+++ src/lib.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
-pub const err = @import("error.zig");
 pub const App = @import("App.zig");
 pub const Arg = @import("Arg.zig");
 pub const ArgsContext = @import("args_context.zig").ArgsContext;
 pub const Command = @import("Command.zig");
+pub const YazapError = @import("error.zig").YazapError;
 
 test "emit docs" {
     std.testing.refAllDeclsRecursive(@This());
