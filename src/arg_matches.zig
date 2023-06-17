ba4bede2c1ada92878693a34d09e086dac549989
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-06-17 19:09:03 +0545

    refactor(arg_matches): Reorder functions of `ArgMatches`
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- src/arg_matches.zig
+++ src/arg_matches.zig
@@ -79,6 +79,29 @@ pub const ArgMatches = struct {
         self.subcommand = alloc_subcmd;
     }
 
+    /// Checks whether any arguments were present on the command line.
+    ///
+    /// ## Examples
+    ///
+    /// ```zig
+    /// var app = App.init(allocator, "myapp", "My app description");
+    /// defer app.deinit();
+    ///
+    /// var root = app.rootCommand();
+    /// try root.addArg(Arg.booleanOption("verbose", 'v', "Enable verbose output"));
+    /// try root.addSubcommand(app.createCommand("init-exe", "Initilize project"));
+    ///
+    /// const matches = try app.parseProcess();
+    ///
+    /// if (!matches.hasArguments()) {
+    ///     try app.displayHelp();
+    ///     return;
+    /// }
+    /// ```
+    pub fn hasArguments(self: *const ArgMatches) bool {
+        return ((self.args.count() >= 1) or (self.subcommand != null));
+    }
+
     /// Checks whether an option, positional argument or subcommand with the
     /// specified name was present on the command line.
     ///
@@ -118,29 +141,6 @@ pub const ArgMatches = struct {
             return std.mem.eql(u8, subcmd.name, name);
         }
         return false;
-    }
-
-    /// Checks whether any arguments were present on the command line.
-    ///
-    /// ## Examples
-    ///
-    /// ```zig
-    /// var app = App.init(allocator, "myapp", "My app description");
-    /// defer app.deinit();
-    ///
-    /// var root = app.rootCommand();
-    /// try root.addArg(Arg.booleanOption("verbose", 'v', "Enable verbose output"));
-    /// try root.addSubcommand(app.createCommand("init-exe", "Initilize project"));
-    ///
-    /// const matches = try app.parseProcess();
-    ///
-    /// if (!matches.hasArguments()) {
-    ///     try app.displayHelp();
-    ///     return;
-    /// }
-    /// ```
-    pub fn hasArguments(self: *const ArgMatches) bool {
-        return ((self.args.count() >= 1) or (self.subcommand != null));
     }
 
     /// Returns the value of an option or positional argument if it was present
