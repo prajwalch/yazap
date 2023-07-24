b4fd4527efabcda69b8737c632ca9a1c1abd1aae
PrajwalCH <prajwal.chapagain58@gmail.com>
2023-07-24 17:14:53 +0545

    fix(#12): Update to latest zig
    Signed-off-by: PrajwalCH <prajwal.chapagain58@gmail.com>

--- build.zig
+++ build.zig
@@ -10,27 +10,28 @@ pub fn build(b: *std.build.Builder) void {
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
 
-    const docs = b.addTest(.{ .root_source_file = .{ .path = "src/lib.zig" } });
-    docs.emit_docs = .emit;
-
+    const docs_test = b.addTest(.{ .root_source_file = .{ .path = "src/lib.zig" } });
+    const install_docs = b.addInstallDirectory(.{
+        .source_dir = docs_test.getOutputDocs(),
+        .install_dir = .prefix,
+        .install_subdir = "docs",
+    });
     const docs_step = b.step("docs", "Generate docs");
-    docs_step.dependOn(&docs.step);
+    docs_step.dependOn(&install_docs.step);
 
     const examples_step = b.step("examples", "Build all the example");
 
     inline for (.{ "git", "touch", "ls" }) |example_name| {
         const example = b.addExecutable(.{
             .name = example_name,
-            .root_source_file = .{ .path = b.fmt("examples/{s}.zig", .{example_name}) },
+            .root_source_file = .{
+                .path = b.fmt("examples/{s}.zig", .{example_name}),
+            },
             .target = target,
             .optimize = optimize,
         });
         const install_example = b.addInstallArtifact(example);
-
         example.addModule("yazap", yazap_mod);
-        example.addAnonymousModule("yazap", .{
-            .source_file = .{ .path = "src/lib.zig" },
-        });
         examples_step.dependOn(&install_example.step);
     }
 }
