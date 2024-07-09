e91b2c03cb15508b61a7f51b295ce9ffcee6d459
prajwalch <prajwal.chapagain58@gmail.com>
2024-07-09 18:45:51 +0545

    chore(build.zig): Update for zig `v0.13.0`
    Signed-off-by: prajwalch <prajwal.chapagain58@gmail.com>

--- build.zig
+++ build.zig
@@ -6,9 +6,14 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const main_tests = b.addTest(.{ .root_source_file = b.path("src/test.zig") });
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/test.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    test_step.dependOn(&run_unit_tests.step);
 
     const docs_test = b.addTest(.{ .root_source_file = b.path("src/lib.zig") });
     const install_docs = b.addInstallDirectory(.{
@@ -30,6 +35,7 @@ pub fn build(b: *std.Build) void {
         });
         const install_example = b.addInstallArtifact(example, .{});
         example.root_module.addImport("yazap", yazap_mod);
+        examples_step.dependOn(&example.step);
         examples_step.dependOn(&install_example.step);
     }
 }
