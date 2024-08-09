8f53fcb3246a5de1c3ce7691bc689c6903206722
fardragon <michaldrozd@protonmail.ch>
2024-08-09 12:24:15 +0200

    Fix compiler crash during 'zig build test'

--- src/lib.zig
+++ src/lib.zig
@@ -4,8 +4,15 @@ pub const Arg = @import("Arg.zig");
 pub const arg_matches = @import("arg_matches.zig");
 pub const ArgMatches = arg_matches.ArgMatches;
 pub const Command = @import("Command.zig");
-pub const YazapError = @import("error.zig").YazapError;
+pub const yazap_error = @import("error.zig");
+pub const YazapError = yazap_error.YazapError;
 
 test "emit docs" {
-    std.testing.refAllDeclsRecursive(@This());
+    comptime {
+        std.testing.refAllDecls(App);
+        std.testing.refAllDecls(Arg);
+        std.testing.refAllDecls(arg_matches);
+        std.testing.refAllDecls(Command);
+        std.testing.refAllDecls(yazap_error);
+    }
 }
